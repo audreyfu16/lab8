05e0f251e22091500a2aae685c04954867086641
Audrey Fu <aufu@ucsd.edu>
2021-02-25 10:54:35 -0800

    got up to slide 25 during lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'hq6XSMZFzFWxQwAzbd7EAUKqUlbJSjHg';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.8785629, -117.2379285],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12
+	});
+
+	//Add a marker to the map
+	L.marker([32.8785629, -117.2379285]).addTo(map);
+
 }
